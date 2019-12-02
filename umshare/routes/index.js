62828fd908afa9b42b18b8f7afd1d05436376c86
Diogo Silva <diogo.silva@xpand-it.com>
2019-12-02 23:10:50 +0000

    Third commit -> First Part of Feed Publications Implementation

--- umshare/routes/index.js
+++ umshare/routes/index.js
@@ -7,7 +7,14 @@ router.get('/', function(req, res) {
 });
 
 router.get('/feed', function(req, res) {
-  res.render('index');
+  var publicacao = {"nome": "Publicacao1", "descricao": "Descricao Publicacao 1", "data": "02/12/2019 21:40"}
+  var publicacao2 = {"nome": "Publicacao2", "descricao": "Descricao Publicacao 2", "data": "30/09/2019 15:20"}
+  var publicacao3 = {"nome": "Publicacao3", "descricao": "Descricao Publicacao 3", "data": "12/07/2019 9:15"}
+  var listaPublicacoes = []
+  listaPublicacoes.push(publicacao)
+  listaPublicacoes.push(publicacao2)
+  listaPublicacoes.push(publicacao3)
+  res.render('index', {publicacoes: listaPublicacoes});
 });
 
 router.get('/register', function(req, res) {
